5100f3d1a92b59aa18e4c9b53ab9abc16f4615c6
Lucas Sotelo <lucasnsotelog@gmail.com>
2024-05-07 22:08:06 -0300

    UI page eventos (#6)
    * creacion de evento
    
    * Tabla de eventos
    
    * Extract component
    
    ---------
    
    Co-authored-by: Joaquin Prada <joaquiprada@gmail.com>

--- src/components/eventos/EventoList.tsx
+++ src/components/eventos/EventoList.tsx
@@ -1,10 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-
-import { cn } from "@/lib/utils";
 import { type Evento, CompleteEvento } from "@/lib/db/schema/eventos";
 import Modal from "@/components/shared/Modal";
 
@@ -12,20 +8,14 @@ import { useOptimisticEventos } from "@/app/(app)/eventos/useOptimisticEventos";
 import { Button } from "@/components/ui/button";
 import EventoForm from "./EventoForm";
 import { PlusIcon } from "lucide-react";
+import { DataTable } from "./DataTable";
+import { columns } from "./columns";
 
 type TOpenModal = (evento?: Evento) => void;
 
-export default function EventoList({
-  eventos,
-   
-}: {
-  eventos: CompleteEvento[];
-   
-}) {
-  const { optimisticEventos, addOptimisticEvento } = useOptimisticEventos(
-    eventos,
-     
-  );
+export default function EventoList({ eventos }: { eventos: CompleteEvento[] }) {
+  const { optimisticEventos, addOptimisticEvento } =
+    useOptimisticEventos(eventos);
   const [open, setOpen] = useState(false);
   const [activeEvento, setActiveEvento] = useState<Evento | null>(null);
   const openModal = (evento?: Evento) => {
@@ -39,14 +29,13 @@ export default function EventoList({
       <Modal
         open={open}
         setOpen={setOpen}
-        title={activeEvento ? "Edit Evento" : "Create Evento"}
+        title={activeEvento ? "Editar Evento" : "Crear Evento"}
       >
         <EventoForm
           evento={activeEvento}
           addOptimistic={addOptimisticEvento}
           openModal={openModal}
           closeModal={closeModal}
-          
         />
       </Modal>
       <div className="absolute right-0 top-0 ">
@@ -57,68 +46,25 @@ export default function EventoList({
       {optimisticEventos.length === 0 ? (
         <EmptyState openModal={openModal} />
       ) : (
-        <ul>
-          {optimisticEventos.map((evento) => (
-            <Evento
-              evento={evento}
-              key={evento.id}
-              openModal={openModal}
-            />
-          ))}
-        </ul>
+        <DataTable columns={columns} data={optimisticEventos} />
       )}
     </div>
   );
 }
 
-const Evento = ({
-  evento,
-  openModal,
-}: {
-  evento: CompleteEvento;
-  openModal: TOpenModal;
-}) => {
-  const optimistic = evento.id === "optimistic";
-  const deleting = evento.id === "delete";
-  const mutating = optimistic || deleting;
-  const pathname = usePathname();
-  const basePath = pathname.includes("eventos")
-    ? pathname
-    : pathname + "/eventos/";
-
-
-  return (
-    <li
-      className={cn(
-        "flex justify-between my-2",
-        mutating ? "opacity-30 animate-pulse" : "",
-        deleting ? "text-destructive" : "",
-      )}
-    >
-      <div className="w-full">
-        <div>{evento.nombre}</div>
-      </div>
-      <Button variant={"link"} asChild>
-        <Link href={ basePath + "/" + evento.id }>
-          Edit
-        </Link>
-      </Button>
-    </li>
-  );
-};
-
 const EmptyState = ({ openModal }: { openModal: TOpenModal }) => {
   return (
     <div className="text-center">
       <h3 className="mt-2 text-sm font-semibold text-secondary-foreground">
-        No eventos
+        No hay eventos
       </h3>
       <p className="mt-1 text-sm text-muted-foreground">
-        Get started by creating a new evento.
+        Empieza creando un nuevo evento.
       </p>
       <div className="mt-6">
         <Button onClick={() => openModal()}>
-          <PlusIcon className="h-4" /> New Eventos </Button>
+          <PlusIcon className="h-4" /> Nuevo Evento{" "}
+        </Button>
       </div>
     </div>
   );
---
+++ src/components/eventos/columns.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { CompleteEvento } from "@/lib/db/schema/eventos";
+import { ColumnDef } from "@tanstack/react-table";
+
+import { MoreHorizontal } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { format } from "date-fns";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export const columns: ColumnDef<CompleteEvento>[] = [
+  {
+    accessorKey: "nombre",
+    header: "Nombre",
+  },
+  {
+    accessorKey: "descripcion",
+    header: "Descripción",
+  },
+  {
+    accessorKey: "fechaInicio",
+    header: "Fecha",
+    cell: ({ row }) => {
+      return format(row.original.fechaInicio as any, "MM/dd/yyyy");
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      return <Actions row={row}/>
+    },
+  },
+];
+
+function Actions({ row }: any) {
+  const pathname = usePathname();
+  const basePath = pathname.includes("eventos")
+    ? pathname
+    : pathname + "/eventos/";
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="h-8 w-8 p-0">
+          <span className="sr-only">Open menu</span>
+          <MoreHorizontal className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>Acciones</DropdownMenuLabel>
+        <DropdownMenuItem>
+          <Link href={basePath + "/" + row.original.id}>Ver evento</Link>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
