60898a32ceddbbe1172d8f96997f466f493728ef
Joaquin Prada <jprada@fi.uba.ar>
2024-05-22 00:49:49 -0300

    Transacciones added
    Co-authored-by: Lucas Sotelo <lsotelo@fi.uba.ar>

--- src/app/(app)/eventos/[eventoId]/deudas/page.tsx
+++ src/app/(app)/eventos/[eventoId]/deudas/page.tsx
@@ -3,7 +3,7 @@ import { notFound } from "next/navigation";
 import { getEventoByIdWithGastos } from "@/lib/api/eventos/queries";
 import { checkAuth } from "@/lib/auth/utils";
 import DeudaList from "@/components/deudas/DeudaList";
-import { getBalancesByEvento } from "@/lib/api/calculadora/queries";
+import { Balance, getBalancesByEvento } from "@/lib/api/calculadora/queries";
 import { log } from "console";
 import BalanceList from "@/components/balances/BalanceList";
 
@@ -21,11 +21,49 @@ export default async function DeudasPage({
   );
 }
 
+// calculate the minumum number of transactions to settle all debts
+const calcularDeudas = (balances: Balance[]) => {
+  // Deep copy balances
+  balances = balances.map(b => ({ ...b }));
+
+  const deudas = balances.filter((b) => b.balance < 0);
+  const prestamos = balances.filter((b) => b.balance > 0);
+
+  deudas.sort((a, b) => a.balance - b.balance);
+  prestamos.sort((a, b) => b.balance - a.balance);
+
+  const transactions: { deudor: string; receptor: string; monto: number }[] = [];
+
+  let i = 0;
+  let j = 0;
+  while (i < deudas.length && j < prestamos.length) {
+    const deuda = deudas[i];
+    const prestamo = prestamos[j];
+
+    const amount = Math.min(-deuda.balance, prestamo.balance);
+    deuda.balance += amount;
+    prestamo.balance -= amount;
+
+    transactions.push({
+      deudor: deuda.nombre,
+      receptor: prestamo.nombre,
+      monto: amount,
+    });
+
+    if (deuda.balance === 0) i++;
+    if (prestamo.balance === 0) j++;
+  }
+
+  return { deudas: transactions };
+}
+
 const Deudas = async ({ id }: { id: string }) => {
   await checkAuth();
 
   const { evento } = await getEventoByIdWithGastos(id);
   const { balances } = await getBalancesByEvento(id);
+  const { deudas } = calcularDeudas(balances);
+
 
   if (!evento) notFound();
   return (
@@ -33,7 +71,7 @@ const Deudas = async ({ id }: { id: string }) => {
       <h3 className="text-xl font-medium mb-4">Balances</h3>
       <BalanceList balances={balances} />
       <h3 className="text-xl font-medium mt-8 mb-4">Deudas</h3>
-      <DeudaList deudas={[]} />
+      <DeudaList deudas={deudas} />
     </div>
   );
 };
