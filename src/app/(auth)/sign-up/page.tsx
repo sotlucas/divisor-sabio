82984ac555c0455714bbc5066d72a374b81b60b5
Lucas Sotelo <lucasnsotelog@gmail.com>
2024-05-29 16:56:25 -0300

    Diseño sign in / sign up (#40)
    * Diseño sign in / sign up
    
    * Spacing

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import Link from "next/link";
 import { useFormState } from "react-dom";
 import { useFormStatus } from "react-dom";
@@ -10,6 +11,13 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import AuthFormError from "@/components/auth/AuthFormError";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default function SignUpPage() {
   const [state, formAction] = useFormState(signUpAction, {
@@ -18,40 +26,56 @@ export default function SignUpPage() {
 
   return (
     <main className="max-w-lg mx-auto my-4 bg-popover p-10">
-      <div className="text-2xl font-bold text-center">
-        <img className="img-center" src="/divisor_sabio.png" style={{margin: "auto"}} alt="logo divisor sabio" />
-        <p>Preparate para que la gestion de tus eventos no tenga mas discusiones!</p>
-      
-        <h1 className="text-2xl font-bold text-center">Bienvenido a Divisor Sabio</h1>
+      <div className="flex flex-col items-center space-y-3 text-center mb-12">
+        <Image
+          className="img-center"
+          src="/divisor_sabio.png"
+          width="193"
+          height="191"
+          alt="logo divisor sabio"
+        />
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          Divisor Sabio
+        </h1>
+        <p className="text-balance text-muted-foreground">
+          ¡No te gastes calculando gastos!
+        </p>
       </div>
-      <br />
-      <br />
-      <h3 className="text-2xl font-bold text-center">Creá tu cuenta</h3>
-      <AuthFormError state={state} />
-      <form action={formAction}>
-        <Label htmlFor="name" className="text-muted-foreground">
-          Nombre
-        </Label>
-        <Input name="name" type="name" id="name" required />
-        <br />
-        <Label htmlFor="email" className="text-muted-foreground">
-          Email
-        </Label>
-        <Input name="email" type="email" id="email" required />
-        <br />
-        <Label htmlFor="password" className="text-muted-foreground">
-          Contraseña
-        </Label>
-        <Input type="password" name="password" id="password" required />
-        <br />
-        <SubmitButton />
-      </form>
-      <div className="mt-4 text-muted-foreground text-center text-sm">
-        ¿Ya tenés cuenta?{" "}
-        <Link href="/sign-in" className="text-secondary-foreground underline">
-          Iniciá sesión
-        </Link>
-      </div>
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-center">Creá tu cuenta</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <AuthFormError state={state} />
+          <form action={formAction}>
+            <div className="grid gap-4">
+              <div className="grid gap-2">
+                <Label htmlFor="name">Nombre</Label>
+                <Input name="name" type="name" id="name" required />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input name="email" type="email" id="email" required />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="password">Contraseña</Label>
+                <Input type="password" name="password" id="password" required />
+              </div>
+              <SubmitButton />
+            </div>
+          </form>
+          <div className="mt-4 text-muted-foreground text-center text-sm">
+            ¿Ya tenés cuenta?{" "}
+            <Link
+              href="/sign-in"
+              className="text-secondary-foreground underline"
+            >
+              Iniciá sesión
+            </Link>
+          </div>
+        </CardContent>
+        <CardFooter></CardFooter>
+      </Card>
     </main>
   );
 }
