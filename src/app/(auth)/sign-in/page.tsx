9dd50a794e9b3832106e3f0d3872a33f9617ef75
Lucas Sotelo <lucasnsotelog@gmail.com>
2024-05-28 22:45:21 -0300

    Diseño sign in / sign up

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import Link from "next/link";
 import { useFormState } from "react-dom";
 import { useFormStatus } from "react-dom";
@@ -10,6 +11,13 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import AuthFormError from "@/components/auth/AuthFormError";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default function SignInPage() {
   const [state, formAction] = useFormState(signInAction, {
@@ -18,36 +26,48 @@ export default function SignInPage() {
 
   return (
     <main className="max-w-lg mx-auto my-4 bg-popover p-10">
-      <div className="text-2xl font-bold text-center">
-        <img className="img-center" src="/divisor_sabio.png" style={{margin: "auto"}} alt="logo divisor sabio" />
-        <h1 className="text-2xl font-bold text-center">Bienvenido a Divisor Sabio</h1>
-        <p>No te gastes calculando gastos</p>
+      <div className="flex flex-col items-center space-y-3 text-center mb-12">
+        <Image
+          className="img-center"
+          src="/divisor_sabio.png"
+          width="193"
+          height="191"
+          alt="logo divisor sabio"
+        />
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+          Divisor Sabio
+        </h1>
+        <p className="text-balance text-muted-foreground">
+          ¡No te gastes calculando gastos!
+        </p>
       </div>
-      <br />
-      <h3 className="text-2xl font-bold text-center">Ingresá a tu cuenta</h3>
-      <AuthFormError state={state} />
-      <form action={formAction}>
-        <Label htmlFor="email" className="text-muted-foreground">
-          Email
-        </Label>
-        <Input name="email" id="email" type="email" required />
-        <br />
-        <Label htmlFor="password" className="text-muted-foreground">
-          Contraseña
-        </Label>
-        <Input type="password" name="password" id="password" required />
-        <br />
-        <SubmitButton />
-      </form>
-      <div className="mt-4 text-sm text-center text-muted-foreground">
-        ¿No tenés cuenta?{" "}
-        <Link
-          href="/sign-up"
-          className="text-accent-foreground underline hover:text-primary"
-        >
-          Crear cuenta
-        </Link>
-      </div>
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-center">Ingresá a tu cuenta</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <AuthFormError state={state} />
+          <form action={formAction}>
+            <Label htmlFor="email">Email</Label>
+            <Input name="email" id="email" type="email" required />
+            <br />
+            <Label htmlFor="password">Contraseña</Label>
+            <Input type="password" name="password" id="password" required />
+            <br />
+            <SubmitButton />
+          </form>
+          <div className="mt-4 text-sm text-center text-muted-foreground">
+            ¿No tenés cuenta?{" "}
+            <Link
+              href="/sign-up"
+              className="text-accent-foreground underline hover:text-primary"
+            >
+              Crear cuenta
+            </Link>
+          </div>
+        </CardContent>
+        <CardFooter></CardFooter>
+      </Card>
     </main>
   );
 }
