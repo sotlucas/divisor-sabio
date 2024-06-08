9c226ca43ce654bf839dd4493b213f5efeaf3de7
Lucas Sotelo <lucasnsotelog@gmail.com>
2024-06-08 19:48:35 -0300

    Empty deudas

--- src/components/deudas/DeudaList.tsx
+++ src/components/deudas/DeudaList.tsx
@@ -3,10 +3,29 @@
 import { DataTable } from "../shared/DataTable";
 import { createColumns } from "./columns";
 
-export default function DeudaList({ deudas, evento, participantes }: { deudas: any; evento: any; participantes: any }) {
+export default function DeudaList({
+  deudas,
+  evento,
+  participantes,
+}: {
+  deudas: any;
+  evento: any;
+  participantes: any;
+}) {
   return (
     <div className="mt-5">
-      <DataTable columns={createColumns(evento, participantes)} data={deudas} />
+      {deudas?.length === 0 ? (
+        <div className="text-center">
+          <p className="mt-1 text-sm text-muted-foreground">
+            Todas las deudas fueron saldadas 🥳
+          </p>
+        </div>
+      ) : (
+        <DataTable
+          columns={createColumns(evento, participantes)}
+          data={deudas}
+        />
+      )}
     </div>
   );
 }
